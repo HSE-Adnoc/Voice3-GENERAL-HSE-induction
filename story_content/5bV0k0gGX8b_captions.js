58ca9f1cfcfb75e87679d250bf2bd5d1ce012c42
HSE-Adnoc <xlexcelpixels@gmail.com>
2026-07-24 11:07:25 +0400

    final updated
    for testing (free menu)

---
+++ story_content/5bV0k0gGX8b_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5bV0k0gGX8b_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind%3A%20captions%0D%0ASource%3A%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version%3A%203.122.37526.0%0D%0A%0D%0A00%3A00%3A00.009%20--%3E%2000%3A00%3A02.390%0D%0AWhat%20is%20the%20purpose%20of%20HSE%20induction%3F%0D%0A%0D%0A"}]})
